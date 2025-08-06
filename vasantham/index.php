330f1fb8dfb034c3ad471c56dcf513ab80855557
sameerahamath <sameer.ahamath@mayuraconsultancy.com>
2025-08-06 22:43:05 +0530

    remove

--- vasantham/index.php
+++ vasantham/index.php
@@ -8,8 +8,7 @@ include('includes/dbconnection.php');
 
 <head>
     
-    <!-- Basic Page Needs
-    <!-- Stylesheets -->
+    
     ============================================= -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i%7CPoppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Stylesheets
